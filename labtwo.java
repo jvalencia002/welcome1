8c213510381e16b3f42bb3fb2def6eee36847d1b
Joey <jvalencia002@hotmail.com>
2016-02-24 20:28:31 -0800

    Added files via upload

---
+++ labtwo.java
@@ -0,0 +1,14 @@
+package labtwo;
+
+/**
+ *
+ * @author Joeyvalencia
+ */
+public class labtwo
+{
+	public static void main( String[] args)
+	{
+		System.out.println("Hello\nJose\nValencia\nyou arecool");
+            
+	}
+}
