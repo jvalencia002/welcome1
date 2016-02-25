6d587ead3b2d6f21f74bdb5af39163ee3e028f4e
Joey <jvalencia002@hotmail.com>
2016-02-24 20:58:54 -0800

    Added files via upload

---
+++ Labthree.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labthree;
+
+/**
+ *
+ * @author Joeyvalencia
+ */
+public class Labthree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        // TODO code application logic here
+        String name = "Hello World" ;
+        System.out.println(name);
+       
+    }
+    
+}
